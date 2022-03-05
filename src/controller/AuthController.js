f16f615ac040cf3362d56981bd5f9f3acbf119fd
Dipronil1998 <dipronildas.net@gmail.com>
2022-03-05 21:15:16 +0530

    forgot and reset password create

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,8 +1,9 @@
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const message = require('../../config/constant');
 const {validationResult} = require('express-validator');
+const sendMail=require('../middleware/mail');
 
 exports.signUp = async (req, res) => {
   try {
@@ -55,5 +56,67 @@ exports.logIn = async (req, res) => {
     }
   } catch (error) {
     res.json({error: 'Invalid Email/Password'});
+  }
+};
+
+exports.sendUserPasswordResetEmail=async (req, res) => {
+  try {
+    const email = req.body.email;
+    if (email) {
+      const user = await User.findOne({email: email});
+      if (user) {
+        const secret = user._id + process.env.JWT_SECRET_KEY;
+        const token = jwt.sign({userID: user._id}, secret, {
+          expiresIn: '15m',
+        });
+        const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`;
+        console.log(link);
+        sendMail({
+          from: process.env.EMAIL,
+          to: user.email,
+          subject: 'Password Reset Link',
+          html: `<h5>Hi ${user.name}</h5><a href=${link}>Click Here</a> 
+          to Reset Your Password`,
+        });
+        res.send({
+          message: 'Password Reset Email Sent... Please Check Your Email',
+        });
+      } else {
+        res.send({message: 'Email doesn\'t exists'});
+      }
+    } else {
+      res.send({message: 'Email Field is Required'});
+    }
+  } catch (error) {
+    res.json({error: 'Something Wrong'});
+  }
+};
+
+exports.userPasswordReset=async (req, res) => {
+  const {password, confirm_password} = req.body;
+  const {id, token} = req.params;
+  const user = await User.findById(id);
+  const new_secret = user._id + process.env.JWT_SECRET_KEY;
+  try {
+    jwt.verify(token, new_secret);
+    if (password && confirm_password) {
+      if (password !== confirm_password) {
+        res.json({
+          message: 'New Password and Confirm New Password doesn\'t match',
+        });
+      } else {
+        const salt =Number(process.env.SALT);
+        const newHashPassword = await bcrypt.hash(password, salt);
+        await User.findByIdAndUpdate(user._id, {
+          $set: {password: newHashPassword},
+        });
+        res.json({message: 'Password Reset Successfully'});
+      }
+    } else {
+      res.json({message: 'All Fields are Required'});
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({message: 'Invalid Token'});
   }
 };
--- src/validator/AuthValidation.js
+++ src/validator/AuthValidation.js
@@ -9,10 +9,11 @@ exports.signInValidator=[
       .normalizeEmail().notEmpty().withMessage('Email should not be empty!'),
   body('password').trim()
       .matches(/^(?=.{8,32})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/)
-      .withMessage('Password should be a alphanumeric and spcial character')
+      .withMessage(`Password should be a alphanumeric,
+      spcial character and mininum 8 Bit long`)
       .notEmpty().withMessage('Password should not be empty!'),
   body('confirm_password').trim()
-      .notEmpty().withMessage('Password should not be empty!'),
+      .notEmpty().withMessage('Confirm Password should not be empty!'),
 ];
 
 exports.logInValidator=[
@@ -22,4 +23,18 @@ exports.logInValidator=[
       .notEmpty().withMessage('Password should not be empty!'),
 ];
 
+exports.forgotPasswordValidator=[
+  body('email').isEmail().withMessage('Please Enter a valid Email')
+      .normalizeEmail().notEmpty().withMessage('Email should not be empty!'),
+];
+
+exports.resetPasswordValidator=[
+  body('password').trim()
+      .matches(/^(?=.{8,32})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/)
+      .withMessage(`Password should be a alphanumeric,
+      spcial character and minium 8 Bit long`)
+      .notEmpty().withMessage('Password should not be empty!'),
+  body('confirm_password').trim()
+      .notEmpty().withMessage('Confirm Password should not be empty!'),
+];
 
