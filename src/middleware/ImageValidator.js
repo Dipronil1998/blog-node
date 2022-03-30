1c2ca5dd69df983ae9fbd0e00e978c2e49012f25
Dipronil1998 <dipronildas.net@gmail.com>
2022-03-30 19:40:52 +0530

    update fixture

--- src/middleware/ImageValidator.js
+++ src/middleware/ImageValidator.js
@@ -12,16 +12,13 @@ const imageValidate = (req, res, next) => {
 
 const imageValidateUpdate = (req, res, next) => {
   if (req.file) {
-    console.log('aaaaa');
     const expectedFileType = ['png', 'jpg', 'jpeg'];
     const fileExtension = req.file.mimetype.split('/').pop();
     if (!expectedFileType.includes(fileExtension)) {
       return res.status(400).json({message: 'Image is not valid'});
     }
     next();
-    console.log('tttttttttttt');
   }
-  console.log('hhhhhhh');
   next();
 };
 
---
+++ test/controller/fixtures/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../../../src/model/user');
+
+const userOne = {
+    // _id: new mongoose.Types.ObjectId(),
+    name: 'Dip Das',
+    email: 'test@example.com',
+    password: '56what!!',
+}
+
+
+const setupDatabase = async () => {
+    await new User(userOne).save();
+}
+
+module.exports = {
+    userOne,
+    setupDatabase
+}
