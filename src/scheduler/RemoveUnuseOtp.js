00ce5524ce73611ade362ebcfaf997509be7400d
Dipronil1998 <dipronilda.net@gmail.com>
2022-09-20 23:34:40 +0530

    cron job bug fixed

--- src/scheduler/RemoveUnuseOtp.js
+++ src/scheduler/RemoveUnuseOtp.js
@@ -1,14 +1,17 @@
 const cron = require('node-cron');
 const moment = require('moment');
 const Otp = require('../model/otp');
+const logger = require('../utils/logger');
 
-exports.otpCron = cron.schedule('20 * 22 19 * *', async () => {
+exports.otpCron = cron.schedule('30 * 23 20 * *', async () => {
   try {
-    const previousTime = moment(new Date()).subtract(1, 'days').format('YYYY-MM-DD hh:mm:ss');
-    // console.log("previousTime: ", previousTime.split(' ')[0]);
-    console.log((new Date(previousTime).toDateString()));
-    const otp = await Otp.find({ gte: { createdAt: new Date(previousTime) } });
-    console.log(otp)
+    const previousTime = moment(new Date()).subtract(15, 'days').format('YYYY-MM-DD hh:mm:ss');
+    // console.log(new Date(new Date(previousTime).toISOString()));
+    const otp = await Otp.deleteMany({ createdAt: { $lte: new Date(new Date(previousTime).toISOString()) } });
+    if(otp.deletedCount>0){
+      console.log(otp.deletedCount+ " OTP deleted");
+      logger.info(otp.deletedCount+ " OTP deleted");
+    }
   } catch (error) {
     console.log(error);
   }
